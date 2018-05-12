74b57ab1ec43daaa2e092cb1983c393f1c65a127
yedpodtrzitko <yed@vanyli.net>
2018-05-12 11:49:58 +0200

    use mypy cache, bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(join(dirname(__file__), 'requirements.txt')) as f:
 
 setup(
     name='releaselock',
-    version='0.4.0',
+    version='0.4.1',
     description='Slack App for managing release mutex',
     classifiers=['Private :: Do Not Upload'],
     packages=find_packages(),
